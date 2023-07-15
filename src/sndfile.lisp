7e0ae2972934cf7a83b33cf6bc39099dca9c1123
Ruben Philipp <me@rubenphilipp.com>
2023-07-15 19:46:56 +0200

    make-sndfile

--- src/sndfile.lisp
+++ src/sndfile.lisp
@@ -20,7 +20,7 @@
 ;;; CLASS HIERARCHY
 ;;; named-object -> sndfile
 ;;;
-;;; $$ Last modified:  19:31:39 Sat Jul 15 2023 CEST
+;;; $$ Last modified:  19:46:47 Sat Jul 15 2023 CEST
 ;;; ****
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -67,6 +67,83 @@
           (path sf) (snd-duration sf) (channels sf) (start sf)
           (end sf) (amplitude sf) (duration sf)
           (description sf)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defmethod (setf path) :after (value (sf sndfile))
+  (declare (ignore value))
+  (update sf))
+
+(defmethod (setf description) :after (value (sf sndfile))
+  (declare (ignore value))
+  (setf (slot-value sf 'description) value)
+(update sf))
+
+(defmethod (setf duration) :after (value (sf sndfile))
+  (declare (ignore value))
+  (setf (slot-value sf 'duration) (mins-secs-to-secs (duration sf)))
+  (when (data-consistent sf)
+    (setf (data-consistent sf) nil)
+    (set-end sf)
+    (update sf)))
+
+(defmethod (setf end) :after (value (sf sndfile))
+  (declare (ignore value))
+  (setf (slot-value sf 'end) (mins-secs-to-secs (end sf)))
+  (when (data-consistent sf)
+    (setf (data-consistent sf) nil)
+    (set-dur sf)
+    (update sf)))
+
+(defmethod (setf start) :after (value (sf sndfile))
+  (declare (ignore value))
+  (setf (slot-value sf 'start) (mins-secs-to-secs (start sf)))
+  (when (data-consistent sf)
+    (setf (data-consistent sf) nil)
+    (set-dur sf)
+    (update sf)))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; ****f* sndfile/make-sndfile
+;;; AUTHOR
+;;; Ruben Philipp <me@rubenphilipp.com>
+;;;
+;;; CREATED
+;;; 2023-07-15
+;;; 
+;;; DESCRIPTION
+;;; This is a helper function in order to create an instance of the
+;;; sndfile-class.
+;;;
+;;; ARGUMENTS
+;;; The path to the sound file.
+;;; 
+;;; OPTIONAL ARGUMENTS
+;;; - :id. An id for the object. Default = NIL
+;;; - :data. Additional data linked to the object. Default = the path of them
+;;;   sndfile
+;;; - :duration. The desired duration of the sndfile (in seconds). This should
+;;;   not be specified if :end has been specified. When neither :end nor
+;;;   :duration is set, the sound file will be considered as having to be
+;;;   considered until its end. Default = NIL
+;;; - :end. The desired end time of the sndfile (in seconds). This should not
+;;;   be specified if :duration has been specified. Default = NIL
+;;; - :start. The desired start time (in seconds). Default = 0.0
+;;; - :amplitude. An amplitude scaler (used in some processes). Default = 1.0
+;;; 
+;;; RETURN VALUE
+;;; The sndfile-object. 
+;;;
+;;; SYNOPSIS
+(defun make-sndfile (path &key id data duration end (start 0.0)
+                            (amplitude 1.0))
+  ;;; ****
+  (make-instance 'sndfile :id id :data data :path path :duration duration
+                 :end end :start start :amplitude amplitude))
+
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
--- tests/tests.lisp
+++ tests/tests.lisp
@@ -12,7 +12,7 @@
 ;;; PURPOSE
 ;;; Regression test suite for klitter.
 ;;;
-;;; $$ Last modified:  19:28:18 Sat Jul 15 2023 CEST
+;;; $$ Last modified:  19:41:55 Sat Jul 15 2023 CEST
 ;;; ****
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -63,6 +63,20 @@
     (is (and
          (= (klitter::sample-rate sndfile) 48000)
          (= (klitter::channels sndfile) 2)))))
+
+;;; test make-sndfile
+;;; RP  Sat Jul 15 19:39:29 2023
+(test test-make-sndfile
+  (let* ((path (test-pathname "snd/kalimba.wav"))
+         (sndfile (klitter::make-sndfile path
+                                         :id "kalimba"
+                                         :start 2.0
+                                         :duration 5.0)))
+    (is (and
+         (= (klitter::start sndfile) 2.0)
+         (equal "kalimba" (klitter::id sndfile))
+         (= (klitter::end sndfile) 7.0)
+         (= (klitter::duration sndfile) 5.0)))))
 
 
 
