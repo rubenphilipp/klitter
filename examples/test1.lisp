8cf7829435c88fdfdb33db23bae81eb3c21d48e3
Ruben Philipp <me@rubenphilipp.com>
2024-07-18 19:21:00 +0200

    deleted old example

--- examples/test1.lisp
+++
@@ -1,7 +0,0 @@
-(in-package :klitter)
-
-(let* ((sndpath (path-from-same-dir "snd/kalimba.wav"))
-       (sndfile (make-instance 'sndfile
-                               :path sndpath)))
-  (print sndfile))
-
